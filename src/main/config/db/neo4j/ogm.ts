aed1bdf5cacf7518a9d2c97400939800ecdc3901
hugo miranda <hugo.miranda.souza@gmail.com>
2022-03-27 21:16:41 -0300

    change paths alias

--- src/main/config/db/neo4j/ogm.ts
+++ src/main/config/db/neo4j/ogm.ts
@@ -1,6 +1,6 @@
-import { ModelMap } from "@infra/db/neo4j/ogm-types";
-import { consolidateTypeDefs } from "@main/api/gql/consolidate-type-defs";
-import { neo4jConnectionFactory } from "@main/factory/main/db/connection.factory";
+import { ModelMap } from "@/infra/db/neo4j/ogm-types";
+import { consolidateTypeDefs } from "@/main/api/gql/consolidate-type-defs";
+import { neo4jConnectionFactory } from "@/main/factory/main/db/connection.factory";
 import { OGM } from "@neo4j/graphql-ogm";
 
 export async function configureNeo4jOGM() {
