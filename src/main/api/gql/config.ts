9fa6176fff7fa46b762776f42dc543eff59d00c3
hugo miranda <hugo.miranda.souza@gmail.com>
2022-03-29 21:13:32 -0300

    add resolvers in Neo4JGraphQL Configuration

--- src/main/api/gql/config.ts
+++ src/main/api/gql/config.ts
@@ -1,12 +1,15 @@
-import { Neo4jGraphQL } from "@neo4j/graphql";
-import { neo4jConnectionFactory } from "@/main/factory/main/db/connection.factory";
-import { consolidateTypeDefs } from "./consolidate-type-defs";
+import { neo4jConnectionFactory } from '@/main/factory/main/db/neo4j/connection.factory';
+import { userResolvers } from '@/modules/auth/gql/resolvers';
+import { Neo4jGraphQL } from '@neo4j/graphql';
+import { consolidateTypeDefs } from './consolidate-type-defs';
+
 
 export async function configureNeo4jGraphQL() {
   const driver = await neo4jConnectionFactory();
   const typeDefs = consolidateTypeDefs();
 
   return new Neo4jGraphQL({
+    resolvers: [userResolvers],
     typeDefs,
     driver,
   });
