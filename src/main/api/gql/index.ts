013e54a95bb28cc7e92465247509645fdd13df1d
hugo miranda <hugo.miranda.souza@gmail.com>
2022-04-09 20:10:02 -0300

    cr: using relative paths; creating barrels

---
+++ src/main/api/gql/index.ts
@@ -0,0 +1,3 @@
+export * from './config';
+export * from './consolidate-type-defs';
+export * from './server';
--- src/main/scripts/generate-types.ts
+++ src/main/scripts/generate-types.ts
@@ -1,7 +1,7 @@
 import { SRC_PATH } from "@/src-directory";
 import { generate } from "@neo4j/graphql-ogm";
 import * as path from "path";
-import { OGMFactory } from "../factory/adapters/neo4j/ogm";
+import { OGMFactory } from "@/main/factory/adapters/neo4j/ogm";
 
 if (process.env.GENERATE === "true") {
   (async () => {
