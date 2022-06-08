cabf417cb4bb9ee1addcabc56cd6fa4bf5cba037
hugo miranda <hugo.miranda.souza@gmail.com>
2022-06-08 20:04:25 -0300

    inject secret and expiresIn in the JwtAdapter

--- src/infra/token/jwt/jwt.adapter.ts
+++ src/infra/token/jwt/jwt.adapter.ts
@@ -4,18 +4,17 @@ import { ApolloError } from "apollo-server-core";
 import * as jwt from "jsonwebtoken";
 
 export class JwtAdapter implements TokenProtocol.Create, TokenProtocol.Verify {
-  private secret: string;
-
-  constructor() {
-    this.secret = Env.JWT_SECRET;
-
+  constructor(
+    private readonly secret: string,
+    private readonly expiresIn: string
+    ) {
     if (!this.secret) {
       throw new ApolloError("Couldn't find JWT_SECRET environment variable");
     }
   }
 
   create(payload: TokenProtocol.PayloadTypes): string {
-    return jwt.sign(payload, this.secret, { expiresIn: "1d" });
+    return jwt.sign(payload, this.secret, { expiresIn: this.expiresIn });
   }
 
   verify<TokenData>(token: string): TokenProtocol.BaseToken<TokenData> {
--- src/main/factory/adapters/token/jwt.factory.ts
+++ src/main/factory/adapters/token/jwt.factory.ts
@@ -1,5 +1,6 @@
 import { JwtAdapter } from "@/infra/token/jwt";
+import { Env } from "@/main/config/system";
 
 export function JwtTokenFactory(){
-  return new JwtAdapter();
+  return new JwtAdapter(Env.JWT_SECRET, Env.JWT_EXPIRES_IN);
 }
