922c586480bd2732d58d6684cb9a14e7405940b7
hugo miranda <hugo.miranda.souza@gmail.com>
2022-06-08 18:49:34 -0300

    export UseCase as interface

--- src/modules/auth/domain/use-case/auth.use-case.ts
+++ src/modules/auth/domain/use-case/auth.use-case.ts
@@ -1,10 +1,8 @@
-import { UseCase } from "@/shared/domain/use-case";
-import { AuthModel } from '@/modules/auth/domain/models/authenticate.model'
+import { UseCase as BaseUseCase } from "@/shared/domain/use-case";
+import { AuthModel } from "@/modules/auth/domain/models/authenticate.model";
 
 export namespace Auth {
   export type Params = AuthModel.Params;
   export type Response = AuthModel.Response;
-
-  export interface UseCase
-    extends UseCase.Implementation<Params, Promise<Response>> {}
+  export type UseCase = BaseUseCase<Params, Promise<Response>>;
 }
