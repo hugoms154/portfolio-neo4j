4fb76a89596c70614f68a7bdd2ee65fea05a71d5
hugo miranda <hugo.miranda.souza@gmail.com>
2022-06-08 20:04:25 -0300

    inject secret and expiresIn in the JwtAdapter

--- src/main/config/system/environment.config.ts
+++ src/main/config/system/environment.config.ts
@@ -2,6 +2,7 @@ interface EnvironmentSchema {
   NODE_ENV: string;
   PORT: number;
   JWT_SECRET: string;
+  JWT_EXPIRES_IN: string;
   NEO4J_URL: string;
   NEO4J_USER: string;
   NEO4J_PASSWORD: string;
@@ -11,6 +12,7 @@ export const Env: EnvironmentSchema = {
   NODE_ENV: process.env.NODE_ENV || "development",
   PORT: Number(process.env.PORT) || 4000,
   JWT_SECRET: process.env.JWT_SECRET || "very-secret",
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1d",
   NEO4J_URL: process.env.NEO4J_URL || "bolt://localhost:7687",
   NEO4J_USER: process.env.NEO4J_USER || "neo4j",
   NEO4J_PASSWORD: process.env.NEO4J_PASSWORD || "123123",
