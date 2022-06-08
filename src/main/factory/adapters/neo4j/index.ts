e031ef08796a17197c6971e87b0f838e8c9d8d03
hugo miranda <hugo.miranda.souza@gmail.com>
2022-06-08 19:00:04 -0300

    remove repository factories, mount repository in use-case factories

--- src/main/factory/adapters/neo4j/index.ts
+++ src/main/factory/adapters/neo4j/index.ts
@@ -1,3 +1,2 @@
 export * from './ogm';
-export * from './user';
 export * from './connection.factory';
--- src/main/factory/adapters/neo4j/user/create-user-repository.factory.ts
+++
@@ -1,9 +0,0 @@
-import { UserRepository } from "@/infra/db/neo4j/repositories";
-import { OGMFactory } from "@/main/factory/adapters/neo4j/ogm";
-import { CreateUserRepository } from "@/modules/user/app/protocols/db/repositories/create.repository";
-
-export async function createUserRepositoryFactory(): Promise<CreateUserRepository> {
-  const ogm = await OGMFactory();
-  
-  return new UserRepository(ogm);
-}
