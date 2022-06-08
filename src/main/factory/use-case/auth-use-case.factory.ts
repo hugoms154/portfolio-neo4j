f26f5f783ef58ccffbab9128ccae15e1567983ac
hugo miranda <hugo.miranda.souza@gmail.com>
2022-06-08 19:42:27 -0300

    rename JWTFactory to JwtTokenFactory

--- src/main/factory/use-case/auth-use-case.factory.ts
+++ src/main/factory/use-case/auth-use-case.factory.ts
@@ -1,9 +1,9 @@
 import { UserRepository } from "@/infra/db/neo4j/repositories";
 import { OGMFactory } from "@/main/factory/adapters/neo4j/ogm";
 import { AuthUseCase } from "@/modules/auth/app/use-case/auth.use-case";
-import { JWTFactory } from "@/main/factory/adapters/jwt";
+import { JwtTokenFactory } from "@/main/factory/adapters/token";
 
 export async function AuthUseCaseFactory(): Promise<AuthUseCase> {
   const findUserRepo = new UserRepository(await OGMFactory());
-  return new AuthUseCase(findUserRepo, JWTFactory());
+  return new AuthUseCase(findUserRepo, JwtTokenFactory());
 }
