d30acc1f7059e7e87504d50d2181fd370ec28583
hugo miranda <hugo.miranda.souza@gmail.com>
2022-06-15 20:10:31 -0300

    inject ogm repository in the factory, avoiding building inside the class

--- src/main/factory/use-case/auth.use-case.factory.ts
+++ src/main/factory/use-case/auth.use-case.factory.ts
@@ -4,6 +4,7 @@ import { AuthUseCase } from "@/modules/auth/app/use-case/auth.use-case";
 import { JwtTokenFactory } from "@/main/factory/adapters/token";
 
 export async function AuthUseCaseFactory(): Promise<AuthUseCase> {
-  const findUserRepo = new UserRepository(await OGMFactory());
+  const ogm = await OGMFactory();
+  const findUserRepo = new UserRepository(ogm.model("User"));
   return new AuthUseCase(findUserRepo, JwtTokenFactory());
 }
