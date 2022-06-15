53be1f2487c7c9228efc2abb0f2594a9d91e9c63
hugo miranda <hugo.miranda.souza@gmail.com>
2022-06-15 19:51:34 -0300

    remove commented code

--- src/modules/auth/domain/gql/auth.resolver.ts
+++ src/modules/auth/domain/gql/auth.resolver.ts
@@ -2,8 +2,6 @@ import { Authentication, MutationAuthArgs } from "@/infra/db/neo4j";
 import { Resolver as BaseResolver } from "@/shared/gql/resolver";
 
 export namespace AuthGraphQL {
-  // export interface Params extends AuthModel.Params {}
-  // export interface Response extends AuthModel.Response {}
   export type Params = MutationAuthArgs;
   export type Response = Authentication;
   export type Resolver = BaseResolver<Params, Promise<Response>>;
