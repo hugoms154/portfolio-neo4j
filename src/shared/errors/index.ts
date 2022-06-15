347a2d8bfc613c6c14e995987b7cb0818060573f
hugo miranda <hugo.miranda.souza@gmail.com>
2022-06-14 21:08:47 -0300

    Revert "create custom errors"
    This reverts commit 151b52570716c8779d3b62f5e4ab99fa751d25b5.

--- src/shared/errors/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './action-failed.error';
-export * from './protocol.error';
-export * from './repository.error';
--- src/shared/errors/protocol.error.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloError } from 'apollo-server-errors';
-
-export class ProtocolError extends ApolloError {
-  constructor(message: string) {
-    super(message, 'PROTOCOL_ERROR');
-
-    Object.defineProperty(this, 'name', { value: 'ProtocolError' });
-  }
-}
