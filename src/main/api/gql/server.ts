abb5e31c0d7c454c584ae30978caa0bbb48d7116
hugo miranda <hugo.miranda.souza@gmail.com>
2022-07-05 20:43:05 -0300

    use ResponseExtensionsPlugin

--- src/main/api/gql/server.ts
+++ src/main/api/gql/server.ts
@@ -4,12 +4,17 @@ import { ApolloServer } from "apollo-server-express";
 import http from "http";
 import { configureNeo4jGraphQL } from "./config";
 import { GraphQLContext } from "./context";
+import { ResponseExtensionsPlugin } from "./plugins";
 
 export async function configureGraphQLServer(httpServer: http.Server) {
   const neoSchema = await configureNeo4jGraphQL();
   return new ApolloServer({
     schema: await neoSchema.getSchema(),
     introspection: true,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ResponseExtensionsPlugin,
+    ],
     context: async ({ req }): Promise<GraphQLContext> => {
       const uuid = UuidFactory();
 
